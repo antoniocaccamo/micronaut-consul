0cee22074f1e429ac4e0f698d1a11eb887b20d9b
antonio caccamo <caccamo.antonio@outlook.com>
2019-09-17 17:37:44 +0200

    micronaut-consul-book

---
+++ micronaut-consul-bookrecommandation/src/main/java/com/github/antoniocaccamo/bookrecommandation/Application.java
@@ -0,0 +1,10 @@
+package com.github.antoniocaccamo.bookrecommandation;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
