958c9d0a8aafbe6b2f3739b912b851b12b8b9493
supportAT <software@arttechonline.com>
2019-10-08 01:51:49 +0200

    add : bookui

--- bookui/src/App.vue
+++ bookui/src/App.vue
@@ -1,36 +1,22 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <v-app>
+        <app-navigation></app-navigation>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+        <v-content transition="slide-x-transition">
+            <router-view></router-view>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import AppNavigation from '@/components/AppNavigation';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
+    name: 'App',
+    components: {
+        AppNavigation
+    }
 };
 </script>
+
+<style></style>
