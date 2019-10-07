3813d1933e2d6203a674d8fee6804497d9c14bcf
supportAT <software@arttechonline.com>
2019-10-08 01:30:59 +0200

    add: compose

--- bookui/src/App.vue
+++ bookui/src/App.vue
@@ -1,22 +1,36 @@
 <template>
-    <v-app>
-        <app-navigation></app-navigation>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
 
-        <v-content transition="slide-x-transition">
-            <router-view></router-view>
-        </v-content>
-    </v-app>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import AppNavigation from '@/components/AppNavigation';
+import HelloWorld from './components/HelloWorld';
 
 export default {
-    name: 'App',
-    components: {
-        AppNavigation
-    }
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style></style>
--- bookui___/src/main.js
+++ bookui___/src/main.js
@@ -4,14 +4,12 @@ import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 const app = new Vue({
     router,
     store,
-    vuetify,
     render: h => h(App)
 }).$mount('#app');
 if (window.Cypress) {
